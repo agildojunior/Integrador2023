f0dac3d388defada803ef53e434d343f0b0a4486
evertonleite <everttonleitte@gmail.com>
2023-01-23 20:27:38 -0300

    Criando apps

--- account/apps.py
+++ account/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class loginRegistroConfig(AppConfig):
+
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'loginRegistro'
+    name = 'account'
--- ifbook/urls.py
+++ ifbook/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from loginRegistro import views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('loginRegistro.urls')),
+    path('auth/', include('core.urls')),
     path('', views.inicio, name='inicio'),
 ]
